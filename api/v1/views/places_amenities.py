88753e686479276cf83921d2e521c759628e1f79
Eric Ubaga <ubaga91@gmail.com>
2023-10-28 20:01:57 +0300

    updated the places_revieews.py file to take in consideration the storage type

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Create a new view for the link between Place objects and Amenity objects"""
 
+from flask import jsonify, abort, request
 from api.v1.views import app_views
 from models import storage
 from models.city import City
@@ -9,7 +10,7 @@ from models.amenity import Amenity
 from models.user import User
 from models.place import Place
 from models.review import Review
-from flask import jsonify, abort, request
+from models import storage_t
 
 
 # Retrieves the list of all Amenity objects of a Place
@@ -21,9 +22,15 @@ def get_amenities(place_id):
     if place is None:
         abort(404)
     amenities = []
-    for amenity in place.amenities:
-        amenities.append(amenity.to_dict())
-    return jsonify(amenities)
+    place_amenities = []
+    if storage_t == 'db':
+        amenities = place.amenities
+    else:
+        amenities = place.amenities()
+    for amenity in amenities:
+        place_amenities.append(amenity.to_dict())
+    return jsonify(place_amenities), 200
+
 
 # delete a Amenity object to a Place
 @app_views.route('/places/<place_id>/amenities/<amenity_id>',
@@ -36,11 +43,18 @@ def delete_amenity(place_id, amenity_id):
     amenity = storage.get(Amenity, amenity_id)
     if amenity is None:
         abort(404)
-    if amenity not in place.amenities:
-        abort(404)
-    place.amenities.remove(amenity)
+    if storage_t == 'db':
+        if amenity not in place.amenities:
+            abort(404)
+        place.amenities.remove(amenity)
+    else:
+        if amenity not in place.amenities():
+            abort(404)
+        place.amenities().remove(amenity)
+
     storage.save()
     return jsonify({}), 200
+
 
 # Link a Amenity object to a Place
 @app_views.route('/places/<place_id>/amenities/<amenity_id>',
@@ -53,8 +67,14 @@ def link_amenity(place_id, amenity_id):
     amenity = storage.get(Amenity, amenity_id)
     if amenity is None:
         abort(404)
-    if amenity in place.amenities:
+    place_amenities = []
+    if storage_t == 'db':
+        place_amenities = place.amenities
+    else:
+        place_amenities = place.amenities()
+
+    if amenity in place_amenities:
         return jsonify(amenity.to_dict()), 200
-    place.amenities.append(amenity)
+    place_amenities.append(amenity)
     storage.save()
     return jsonify(amenity.to_dict()), 201
